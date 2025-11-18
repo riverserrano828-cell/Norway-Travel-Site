ad8f882713daaa382786b02840a0ee2e6c8ffb5b
riverserrano828-cell <riverserrano828@gmail.com>
2025-11-18 06:38:19 -0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -10,7 +10,17 @@ if("serviceWorker" in navigator) {
     window.addEventListener("load", () => {
         navigator.serviceWorker.register("sw.js").then(swReg =>{
             console.log("Worker Registered");
-
+    if('Notification' in window) {
+        console.log('Notifications Supported');
+        
+        Notification.requestPermission(function(status) {
+            console.log('Notification Permission: ', status);
+            
+            if(status === 'granted'){
+                new Notification('Norway Travel');
+            }
+        });
+    }
         }).catch(error => {
             console.log("There was an error",error);
         });
